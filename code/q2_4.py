5647fe20d0ceca0a01c791c2d1ed1ff8bd03a762
Your Name <patirck.heenan@colorado.edu>
2015-04-29 19:20:07 -0600

    added checkpointing. added in histogram basic saving and copy of histogram binary at this point. still getting kHat wrong...

--- code/q2_4.py
+++ code/q2_4.py
@@ -25,36 +25,57 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 from Bio import pairwise2
 from collections import Counter
+# parallelize the dynamic programming stuff.
+from multiprocessing import Pool
 
-#return a golbal alignment for the two sequences
-def globalAlign(humSeq, ratSeq, match=2, mismatch=-1, gapStart=-.5, gapExt=-.1):
+
+#return a global alignment for *translated* version of two nucleotide sequences
+def globalAlign(humSeq, ratSeq, match=2, mismatch=-3, gapStart=-11, gapExt=-2):
+# gap start/extension are defaults from: http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&PROG_DEF=blastn&BLAST_PROG_DEF=blastn&BLAST_SPEC=GlobalAln&LINK_LOC=BlastHomeLink
+# match/mismatch from defaults here: http://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=blastn&BLAST_PROGRAMS=blastn&PAGE_TYPE=BlastSearch&BLAST_SPEC=GlobalAln&LINK_LOC=blasttab&LAST_PAGE=blastp&BLAST_INIT=GlobalAln
   #scores: match, mismatch, gap start, gap extend
-  return pairwise2.align.globalms(humSeq, ratSeq, match, mismatch, gapStart, gapExt)
+    humTx = str(Seq(humSeq).translate())
+    ratTx = str(Seq(ratSeq).translate())
+    return pairwise2.align.globalms(humTx, ratTx, 
+                                    match, mismatch, gapStart,
+                                    gapExt, one_alignment_only=True)
 
 #generate n shuffled versions of humSeq (a biopython Seq object)
 #for each, find optimal alignment to ratSeq (DNA)
 #given: Seq, Seq 
 #return: [alignment scores]
-def shuffledAligns(humSeq, ratSeq,  n):
+def getShuffleAlign(args):
+  i,humStr,ratStr = args
+  alignment = globalAlign(humStr, ratStr)
+  print("Global alignment: iteration... {:d} got score {:.5g}".\
+        format(i,getAlignScore(alignment)))
+  return alignment
+
+def shuffledAligns(humSeq, ratSeq, n,nProc=10):
   alignments = []
   humList = list(humSeq)
   ratStr = str(ratSeq)
-  for _ in xrange(n):
-    np.random.shuffle(humList)
-    alignments.append(globalAlign(''.join(humList), ratStr)[0][2])#score is the third element of the tuple
+  p = Pool(nProc)
+  # bit of a kludge; parallelization (apparently) messed up the pseudo random
+  # number generator. just call shuffle again and save. works fine for
+  # O(10^4) trials.
+  shuffles = [ ''.join(humList) for i in range(n) 
+               if not np.random.shuffle(humList)]
+  # omg python is so awesome. make a pool of processes, map to our function
+  # only wonky bit: have to pass the arguments as a tuple
+  alignments = p.map(getShuffleAlign,[ (i,shuffles[i],ratStr) 
+                                        for i in range(n)] )
   return alignments
 
 #determine the fraction of alignments from shuffledAligns with higher scores
 #than the alignment between the two sequences
 #n is the number of shuffled sequences to create
 def getPValue(humSeq, ratSeq, n=1000):
-  opt = globalAlign(str(humSeq), str(ratSeq))[0][2]
-  scores = shuffledAligns(humSeq, ratSeq, n)
+  opt = getAlignScore(globalAlign(str(humSeq), str(ratSeq)))
+  alignments = shuffledAligns(humSeq, ratSeq, n)
+  scores = [getAlignScore(a) for a in alignments]
   highScores = len(filter(lambda x: x>opt, scores))
   return float(highScores)/n
-
-#quick test
-print(getPValue(Seq("accgt"),Seq("acg"),n=10))
 
 def getCDS(record):
     # more or less copied from here:
@@ -112,7 +133,8 @@ def saveTranslatedNucleotides(files,labels,dataDir,fileEx,printV=True):
             record = SeqRecord(Seq(tx,IUPAC.protein),
                    id=f)
             SeqIO.write(record, fh, "fasta")
-        seqToAlign.append(seq)
+        # get the coding sequence and its translation
+        seqToAlign.append(seq[cds])
         seqTxToAlign.append(tx)
     return seqToAlign,seqTxToAlign
     
@@ -124,67 +146,61 @@ def getProportionsAtIdx(idx,seq,chars,normalize):
     props = counts / normalize
     return props
 
-def getOptimalAminoAcidAlignment(nucleo1,nucleo2):
-    tx1 = str(Seq(nucleo1))
-    tx2 = str(Seq(nucleo2))
-    print(tx1)
-    print(tx2)
-    alignments = pairwise2.align.globalxx(tx1, tx2)
-    print('aligned!')
-    bestAlignment = alignments[0]
-    # return the actual strings associated with the alignment
-    # dont necessarily care about the scores...
-    return bestAlignment[0],bestAlignment[1]
 
 def getMatchIdx(string1,string2):
-    gapOffset1 = 0
-    gapOffset2 = 0
-    matchIdx1 = []
-    matchIdx2 = []
-    misMatchIdx1 = []
-    misMatchIdx2 = []
-    gapIdx1 = []
-    gapIdx2 = []
+  # get where the matches and mismatches between string1 and string2
+  # (both from global alignment) occur, returning match/mismatch/gaps 
+  # for string1
+    gapOffset = 0
+    matchIdx = []
+    misMatchIdx = []
+    gapIdx = []
     gapStr = '-'
     # XXX assume the sequences are of the same length.
     for idx,(char1,char2) in enumerate(zip(string1,
                                            string2)):
-        effIdx1 = idx - gapOffset1
-        effIdx2 = idx - gapOffset2
+        effIdx1 = idx - gapOffset
         if (char1 == char2):
-            matchIdx1.append(effIdx1)
-            matchIdx2.append(effIdx2)
+            matchIdx.append(effIdx1)
         elif (char1 == gapStr or char2 == gapStr):
-            # one was a gap
-            if (char1 == gapStr):
-                gapOffset1 += 1
-            else:
-                gapOffset2 += 1
-            gapIdx1.append(effIdx1)
-            gapIdx2.append(effIdx1)
+            gapIdx.append(effIdx1)
         else:
             # simple mismatch
-            misMatchIdx1.append(effIdx1)
-            misMatchIdx2.append(effIdx1)
+            misMatchIdx.append(effIdx1)
     # XXX for now, just return the mathc indices of the first
-    return matchIdx1,misMatchIdx1,gapIdx1
-            
-    
-def getNonGapProportions(nucleo1,nucleo2,chars):
-    txAlign1,txAlign2 = getOptimalAminoAcidAlignment(nucleo1,nucleo2)
-    matchIdx1,misMatchIdx1,gapIdx1 = getMatchIdx(txAlign1,txAlign2)
+    return matchIdx,misMatchIdx,gapIdx
+                
+def getAlignScore(alignment):
+  # return the first element (best), second score
+  return alignment[0][2]
+
+def getNonGapProportions(nucleo1,nucleo2,chars,forceReAlign):
+  # translate the nucleotides, then globally align them.
+    alignObj =pCheckUtil.getCheckpoint("./out/alignment.pkl",
+                                       globalAlign,forceReAlign,nucleo1,nucleo2)
+    # get the first (only) alignment score
+    alignS1S2ScoreStartEnd = alignObj[0]
+    txAlign1,txAlign2 = alignS1S2ScoreStartEnd[0],alignS1S2ScoreStartEnd[1]
+    alignScore = getAlignScore(alignObj)
+    matchIdx,misMatchIdx,gapIdx = getMatchIdx(txAlign1,txAlign2)
+    matchOrMisMatchIdx = matchIdx + misMatchIdx
     # POST: know the matching / mismatching codons.
     # next, need to figure out 
     # make sure there are no duplicates
-    assert (len(matchIdx1) + len(misMatchIdx1) + len(gapIdx1) == len(nucleo1))
+    nAmino = len(txAlign1)
+    nMatch = len(matchIdx)
+    nMisMatch = len(misMatchIdx)
+    nGap = len(gapIdx)
+    fullLen = (nMatch + nMisMatch + nGap)
+    assert (fullLen == nAmino) , \
+               "Lengths {:d}/{:d} don't match".format(fullLen,nAmino)
     # make sure the indices cover everything
-    assert (set(matchIdx1 +  misMatchIdx1 + gapIdx1) ==  
-            set([i for i in range(len(nucleo1))]) )
+    assert (set(matchIdx +  misMatchIdx + gapIdx) ==  
+            set([i for i in range(nAmino)]) )
     # make sure the indices have no overlap
-    assert (set(matchIdx1) & set(mismatchIdx) & set(gapIdx1)) == set()
+    assert (set(matchIdx) & set(misMatchIdx) & set(gapIdx)) == set()
     # normalize by the total number of nucleotides at codon 'i' (equiv to
     # total number of amino acids)
-    nAminoAcids = maxStrLen-nGaps
     codonSize = 3
     # Pi_a
     propTotal = np.zeros((codonSize,len(chars)))
@@ -193,20 +209,20 @@ def getNonGapProportions(nucleo1,nucleo2,chars):
     idxTx = lambda x,offset: np.array(x)*codonSize+offset
     for offset in range(codonSize):
         # transform the index into what we want
-        allIdx = idxTx(matchOrMisIdx,offset)
-        propTotal[offset,:] = getProportionsAtIdx(allIdx,alignToNucleo,
-                                                  chars,nAminoAcids)
-        misIdx = idxTx(mismatchIdx,offset)
+        allIdx = idxTx(matchOrMisMatchIdx,offset)
+        propTotal[offset,:] = getProportionsAtIdx(allIdx,nucleo1,
+                                                  chars,nAmino)
+        misIdx = idxTx(misMatchIdx,offset)
         # use a normalization of one, so we just get D 
-        dMismatch[offset,:] = getProportionsAtIdx(misIdx,alignToNucleo,
+        dMismatch[offset,:] = getProportionsAtIdx(misIdx,nucleo1,
                                                      chars,1)
-        
     dTotal = np.sum(dMismatch,axis=1)
-    print("{:d} gaps".format(nGaps))
-    print("{:d} matches".format(len(matchIdx)))
-    print("{:d} mismatches".format(len(mismatchIdx)))
-    print("{:d} total matches and mismatches".format(nMatchMisMatch))
-    return propTotal,dTotal,nMatchMisMatch
+    nNonGap = nMatch+nMisMatch
+    print("{:d} gaps".format(nGap))
+    print("{:d} matches".format(nMatch))
+    print("{:d} mismatches".format(nMisMatch))
+    print("{:d} total matches and mismatches".format(nMatch+nMisMatch))
+    return propTotal,dTotal,nNonGap,alignScore
 
 def printAminoInfo(piA,chars):
     delim = "\t\t"
@@ -215,12 +231,12 @@ def printAminoInfo(piA,chars):
         print("Position {:d}\t".format(i) + 
               delim.join("{:.4g}".format(r) for r in row))
 
+
 def get1981ModelCodonPos(piA,D,length):
     # piA is the proportion of base, row for each codon position, 
     # column for each {A,T,G,C}
     # D is the total count of bases. 
     codonSize = 3
-    print(piA)
     H = np.zeros(codonSize)
     H[:] = np.sum(piA * (1-piA),axis=1) # use [:] to make sure no funny indexing
     xVals = D/(H)
@@ -255,8 +271,14 @@ def plotAll(outDir,gXBar,normalStd):
         print("{:d}\t{:.3g}({:.3g})\t{:.3g}".format(i,measured,
                                                     theoryStd**2,theoryStd))
     
+
+def plotAlignments(alignments,score):
+  pass
+
 if __name__ == '__main__':
     dataDir = "../data/"
+    # use the same seed always[!]
+    np.random.seed(42) 
     outDir = "./out/"
     pGenUtil.ensureDirExists(outDir)
     files = ['sequence_human',
@@ -269,7 +291,9 @@ if __name__ == '__main__':
     determineMSA = True
     determineTx = True
     printPiA = True
+    forceAlignments = False
     seqAlignFile = "tx_align.fasta"
+    histogramSize = 10000
     # Posada, Selecting models of evolution, Chapter 10, Figure 10.3
     # If we model serum albumin as ~ ...
     # tau < 100 Millon years (mammals)
@@ -278,23 +302,27 @@ if __name__ == '__main__':
     # residues. so 0.5 /tau residue per unit time
     lambdaV = (50/100) * (1/tau)
     # generate the files for nucleotides translated (tx) coding sequences
-    if determineTx:
-        cdsSeq,cdsTx = saveTranslatedNucleotides(files,fileLabels,dataDir,
-                                                 fileEx)
-    if determineMSA:
-        # choose the index for human...
-        idxFirst = fileLabels.index('human')
-        idxSecond = fileLabels.index('rat')
-        # get piA, the number of proportion of each matched or mismatched
-        # nucleic acid, D, and l fo the Felsenstein 1981 model
-        piA,dMismatch,lenV = getNonGapProportions(cdsSeq[idxFirst],
-                                                  cdsSeq[idxSecond],
-                                                  chars)
-        gXBar,normalStd = get1981ModelCodonPos(piA,dMismatch,lenV)
-        plotAll(outDir,gXBar,normalStd)
+    workDir = "./tmp/"
+    cdsSeq,cdsTx = pCheckUtil.getCheckpoint(workDir + "seq.pkl",\
+                                            saveTranslatedNucleotides,\
+                                            determineTx,files,fileLabels,
+                                            dataDir,fileEx)
+    ratSeq = cdsSeq[fileLabels.index('rat')]
+    humanSeq = cdsSeq[fileLabels.index('human')]
+    # get piA, the number of proportion of each matched or mismatched
+    # nucleic acid, D, and l fo the Felsenstein 1981 model
+    piA,dMismatch,lenV,score = \
+        pCheckUtil.getCheckpoint(workDir + "align.pkl",\
+                                 getNonGapProportions,determineMSA,
+                                 humanSeq,ratSeq,chars,determineMSA)
+    gXBar,normalStd = get1981ModelCodonPos(piA,dMismatch,lenV)
+    plotAll(outDir,gXBar,normalStd)
+    alignments = pCheckUtil.getCheckpoint(workDir + "alignHistogram.pkl",\
+                                          shuffledAligns,forceAlignments,
+                                          humanSeq,ratSeq,histogramSize)
+    plotAlignments(alignments,score)
     if (printPiA):
         printAminoInfo(piA,chars)
-
 
         
 
